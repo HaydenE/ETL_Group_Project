0eaab5d5064a0eafc3338a7f69a82a1af2818968
CharlesPBiggar <charles.p.biggar@gmail.com>
2019-10-19 10:18:02 -0400

    split prescriber info into prescriber info and prescriptions and added new schema

--- table_schema.sql
+++ table_schema.sql
@@ -2,33 +2,53 @@
 
 --Opioid Table
 CREATE TABLE opioids (
-    drug_id INT PRIMARY KEY
-    drug_name VARCHAR(30)
+    drug_id INT PRIMARY KEY,
+    drug_name VARCHAR(30),
     generic_name VARCHAR (30)
 );
 
 -- Overdoses Table
 CREATE TABLE overdoses (
-    state_id INT
-    deaths INT
+    state_id INT,
+    deaths INT,
     FOREIGN KEY (state_id) REFERENCES states(state_id)
 );
 
 -- State Table
 CREATE TABLE states (
-    state_id INT PRIMARY KEY
-    state_abbv VARCHAR(30)
-    state_name VARCHAR(30)
+    state_id INT PRIMARY KEY,
+    state_abbv VARCHAR(30),
+    state_name VARCHAR(30),
     state_pop INT
-)
+);
 
 -- Doctor Table
 CREATE TABLE prescribers (
-    doctor_id INT PRIMARY KEY
-    doctor_type VARCHAR (60)
-    state_id INT
-    drug_id INT
-    gender VARCHAR (1)
-    FOREIGN KEY (state_id) REFERENCES states(state_id)
-    FOEIGN KEY (drug_id) REFERENCES opiods(drug_id)
-)
+    doctor_id INT PRIMARY KEY,
+    doctor_type VARCHAR (60),
+    state_id INT,
+    drug_id INT,
+    gender VARCHAR (1),
+    FOREIGN KEY (state_id) REFERENCES states(state_id),
+    FOREIGN KEY (drug_id) REFERENCES opiods(drug_id)
+);
+
+-- Prescriptions Table
+CREATE TABLE prescriptions (
+    doctor_id INT,
+    ACETAMINOPHEN_CODEINE INT,
+    DIPHENOXYLATE_ATROPINE INT,
+    FENTANYL INT,
+    HYDROCODONE_ACETAMINOPHEN INT,
+    HYDROMORPHONE_HCL INT,
+    METHADONE_HCL INT,
+    MORPHINE_SULFATE INT,
+    MORPHINE_SULFATE.ER INT,
+    OXYCODONE_ACETAMINOPHEN INT,
+    OXYCODONE_HCL INT,
+    OXYCONTIN INT,
+    TRAMADOL_HCL INT,
+    Total_Opioid_Prescriptions INT,
+    Opioid_Prescriber VARCHAR (3),
+    FOREIGN KEY (doctor_id) REFERENCES prescribers(doctor_id)
+);
